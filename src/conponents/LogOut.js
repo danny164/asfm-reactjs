cd9b1b9656ab12d42867880fa92a36e7ced0fcfa
DANNY <qnv164@gmail.com>
2021-04-18 21:07:38 +0700

    Initialize project

--- src/conponents/LogOut.js
+++ src/conponents/LogOut.js
@@ -1,35 +1,28 @@
 import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 
-
 function Signout() {
-
-    const [ setError] = useState('');
-    const {  logout } = useAuth();
+    const [error, setError] = useState('');
+    const { logout } = useAuth();
     const history = useHistory();
 
     async function handleLogout() {
-        setError('')
+        setError('');
         try {
-            await logout()
-            history.push("/login")
+            await logout();
+            history.push('/login');
         } catch {
-            setError('failed to logout !')
+            setError('failed to logout !');
+            console.log(error);
         }
     }
 
     return (
-        <a
-            href="#"
-            className="btn btn-hover-light-primary font-weight-bold py-3 px-6 mb-2 text-center btn-block"
-            onClick={handleLogout}
-        >
+        <Link to="#" className="btn btn-hover-light-primary font-weight-bold py-3 px-6 mb-2 text-center btn-block" onClick={handleLogout}>
             <i className="fad fa-sign-out mr-1" />
             Đăng xuất
-        </a>
-
-
+        </Link>
     );
 }
 
