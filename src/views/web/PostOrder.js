521dc43acaf3ee185b7728bd51e9fe7fb3459f54
DANNY <qnv164@gmail.com>
2021-05-14 22:58:39 +0700

    update

--- src/views/web/PostOrder.js
+++ src/views/web/PostOrder.js
@@ -1,19 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router';
+import googleMapsApi from '../../api/googleMapsApi';
+import AsideLeft from '../../conponents/pages/AsideLeft';
 import AsideRight from '../../conponents/pages/AsideRight';
 import MainPostOrder from '../../conponents/pages/MainPostOrder';
-import AsideLeft from '../../conponents/pages/AsideLeft';
 import { useAuth } from '../../context/AuthContext';
 import { db, realtime } from '../../firebase';
-import queryString from 'query-string';
-import axios from 'axios';
-import googleMapsApi from '../../api/googleMapsApi';
 
 function PostOrder(props) {
     const { currentUser } = useAuth();
     const history = useHistory();
     const [defaultAddressError, setDefaultAddressError] = useState();
-    const [mapAPI, setMapAPI] = useState();
 
     let tamung = '0';
     let address = '';
@@ -27,21 +24,79 @@ function PostOrder(props) {
         detailAddress: '',
     });
 
-    // async function getLngLatAndDistance(startLocation, endLocation) {
-    //     await axios
-    //         .get(
-    //             `https://maps.googleapis.com/maps/api/directions/json?origin=${startLocation}&destination=${endLocation}&key=AIzaSyAZh8iua6hndIGaIWcodmhUEHmX2-QBjrg`,
-    //             config
-    //         )
-    //         .then((res) => {
-    //             console.log(res);
-    //             setMapAPI(res.routes[0]);
-    //         });
-    // }
+    const fetchLngLat = async (startPoint, endPoint, dataPostOrder) => {
+        let lngLatList = await googleMapsApi.getAll(startPoint, endPoint);
 
-    const fetchLngLat = async (startPoint, endPoint) => {
-        const lngLatList = await googleMapsApi.getAll(startPoint, endPoint);
-        console.log(lngLatList);
+        try {
+            //tao bảng newsfeed
+            await realtime.ref('newsfeed/' + dataPostOrder.idPost).set({
+                id_post: dataPostOrder.idPost,
+                noi_giao: dataPostOrder.noi_giao,
+                noi_nhan: address,
+                ghi_chu: dataPostOrder.ghi_chu,
+                km: lngLatList.data.routes[0].legs[0].distance.text,
+                thoi_gian: dataPostOrder.thoi_gian,
+                sdt_nguoi_nhan: dataPostOrder.sdt_nguoi_nhan,
+                ten_nguoi_nhan: dataPostOrder.ten_nguoi_nhan,
+                sdt_nguoi_gui: userInfor.phone,
+                ten_nguoi_gui: userInfor.fullname,
+                phi_giao: dataPostOrder.phi_giao,
+                phi_ung: tamung,
+                id_shop: currentUser.uid,
+                status: '',
+                receiveLng: lngLatList.data.routes[0].legs[0].start_location.lng,
+                receiveLat: lngLatList.data.routes[0].legs[0].start_location.lat,
+                shipLng: lngLatList.data.routes[0].legs[0].end_location.lng,
+                shipLat: lngLatList.data.routes[0].legs[0].end_location.lat,
+            });
+
+            //tạo bảng orderstatus
+            await realtime.ref('OrderStatus/' + currentUser.uid + '/' + dataPostOrder.idPost).set({
+                id_post: dataPostOrder.idPost,
+                id_shop: currentUser.uid,
+                status: '0',
+                noi_giao: dataPostOrder.noi_giao,
+                noi_nhan: address,
+                ghi_chu: dataPostOrder.ghi_chu,
+                km: lngLatList.data.routes[0].legs[0].distance.text,
+                thoi_gian: dataPostOrder.thoi_gian,
+                sdt_nguoi_nhan: dataPostOrder.sdt_nguoi_nhan,
+                ten_nguoi_nhan: dataPostOrder.ten_nguoi_nhan,
+                sdt_nguoi_gui: userInfor.phone,
+                ten_nguoi_gui: userInfor.fullname,
+                phi_giao: dataPostOrder.phi_giao,
+                phi_ung: tamung,
+                ma_bi_mat: dataPostOrder.ma_bi_mat,
+            });
+
+            //tạo bảng transaction
+            await realtime.ref('Transaction/' + dataPostOrder.idPost).set({
+                id_post: dataPostOrder.idPost,
+                id_shop: currentUser.uid,
+                id_shipper: '',
+                id_roomchat: dataPostOrder.id_roomchat,
+                status: '0',
+                ma_bi_mat: dataPostOrder.ma_bi_mat,
+                thoi_gian: dataPostOrder.thoi_gian,
+            });
+
+            //tạo bảng thông báo
+            await realtime
+                .ref('Notification/' + currentUser.uid)
+                .push()
+                .set({
+                    id_post: dataPostOrder.idPost,
+                    id_shop: currentUser.uid,
+                    id_shipper: '',
+                    status: '0',
+                    thoi_gian: dataPostOrder.thoi_gian,
+                });
+
+            //tạo bảng chatroom
+            history.push('/home');
+        } catch (error) {
+            console.log(error);
+        }
     };
 
     //post order function
@@ -63,78 +118,7 @@ function PostOrder(props) {
                 tamung = dataPostOrder.phi_ung;
             }
 
-            fetchLngLat(address, dataPostOrder.noi_giao);
-
-            try {
-                //tao bảng newsfeed
-                await realtime.ref('newsfeed/' + dataPostOrder.idPost).set({
-                    id_post: dataPostOrder.idPost,
-                    noi_giao: dataPostOrder.noi_giao,
-                    noi_nhan: address,
-                    ghi_chu: dataPostOrder.ghi_chu,
-                    // km: mapAPI.legs[0].distance.text,
-                    thoi_gian: dataPostOrder.thoi_gian,
-                    sdt_nguoi_nhan: dataPostOrder.sdt_nguoi_nhan,
-                    ten_nguoi_nhan: dataPostOrder.ten_nguoi_nhan,
-                    sdt_nguoi_gui: userInfor.phone,
-                    ten_nguoi_gui: userInfor.fullname,
-                    phi_giao: dataPostOrder.phi_giao,
-                    phi_ung: tamung,
-                    id_shop: currentUser.uid,
-                    status: '',
-                    // receiveLng: mapAPI.legs[0].start_location.lng,
-                    // receiveLat: mapAPI.legs[0].start_location.lat,
-                    // shipLng: mapAPI.legs[0].end_location.lng,
-                    // shipLat: mapAPI.legs[0].end_location.lat,
-                });
-
-                //tạo bảng orderstatus
-                await realtime.ref('OrderStatus/' + currentUser.uid + '/' + dataPostOrder.idPost).set({
-                    id_post: dataPostOrder.idPost,
-                    id_shop: currentUser.uid,
-                    status: '0',
-                    noi_giao: dataPostOrder.noi_giao,
-                    noi_nhan: address,
-                    ghi_chu: dataPostOrder.ghi_chu,
-                    // km: mapAPI.legs[0].distance.text,
-                    thoi_gian: dataPostOrder.thoi_gian,
-                    sdt_nguoi_nhan: dataPostOrder.sdt_nguoi_nhan,
-                    ten_nguoi_nhan: dataPostOrder.ten_nguoi_nhan,
-                    sdt_nguoi_gui: userInfor.phone,
-                    ten_nguoi_gui: userInfor.fullname,
-                    phi_giao: dataPostOrder.phi_giao,
-                    phi_ung: tamung,
-                    ma_bi_mat: dataPostOrder.ma_bi_mat,
-                });
-
-                //tạo bảng transaction
-                await realtime.ref('Transaction/' + dataPostOrder.idPost).set({
-                    id_post: dataPostOrder.idPost,
-                    id_shop: currentUser.uid,
-                    id_shipper: '',
-                    id_roomchat: dataPostOrder.id_roomchat,
-                    status: '0',
-                    ma_bi_mat: dataPostOrder.ma_bi_mat,
-                    thoi_gian: dataPostOrder.thoi_gian,
-                });
-
-                //tạo bảng thông báo
-                await realtime
-                    .ref('Notification/' + currentUser.uid)
-                    .push()
-                    .set({
-                        id_post: dataPostOrder.idPost,
-                        id_shop: currentUser.uid,
-                        id_shipper: '',
-                        status: '0',
-                        thoi_gian: dataPostOrder.thoi_gian,
-                    });
-
-                //tạo bảng chatroom
-                history.push('/home');
-            } catch (error) {
-                console.log(error);
-            }
+            fetchLngLat(address, dataPostOrder.noi_giao, dataPostOrder);
         }
     }
 
