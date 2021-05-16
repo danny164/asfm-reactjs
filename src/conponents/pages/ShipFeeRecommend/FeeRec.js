93d7b90d8e3bd0f3725f73650e31e93496dbf801
= <thang.huynh304@gmail.com>
2021-05-16 21:30:49 +0700

    test commit

---
+++ src/conponents/pages/ShipFeeRecommend/FeeRec.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+FeeRec.propTypes = {
+    km: PropTypes.number
+};
+
+FeeRec.defaultProps = {
+    km: 0
+}
+function FeeRec(props) {
+    
+
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default FeeRec;
