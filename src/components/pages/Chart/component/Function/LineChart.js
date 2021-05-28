074fc1426be9dc4b03396ed2e6a5d8f0d3ed87a1
= <thang.huynh304@gmail.com>
2021-05-28 10:04:06 +0700

    fixing chart page

--- src/components/pages/Chart/component/Function/LineChart.js
+++ src/components/pages/Chart/component/Function/LineChart.js
@@ -31,7 +31,7 @@ export const lastDataLineChart = (datas, days, setLastData) => {
     } else {
         array = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     }
-    for (let i = 7; i < days.length; i++) {
+    for (let i = 0; i < days.length; i++) {
         datas.map((data) => {
             if (moment.unix(data.thoi_gian).format('DD/MM') === days[i]) {
                 array[i] = array[i] + 1
--- src/components/pages/Chart/component/Function/VerticalChart.js
+++ src/components/pages/Chart/component/Function/VerticalChart.js
@@ -31,7 +31,7 @@ export const lastDataVerticalChart = (datas, days, setLastData) => {
     } else {
         array = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     }
-    for (let i = 7; i < days.length; i++) {
+    for (let i = 0; i < days.length; i++) {
         datas.map((data) => {
             if (moment.unix(data.thoi_gian).format('DD/MM') === days[i]) {
                 array[i] = array[i] + parseInt((data.phi_ung).replace(" ", ''))
